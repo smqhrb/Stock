0937d30cd851f7bbc4e3279fdfc74ec9058850ff
smqhrb <smqhrb@163.com>
2019-02-17 08:34:09 +0800

    year report to class

--- DevelopStock/tools/readYearReport.py
+++ DevelopStock/tools/readYearReport.py
@@ -5,55 +5,162 @@ down load year report
 import urllib.request 
 import re 
 import os 
+import pandas as pd
 URL_YEAR_BASE ='http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/%s/page_type/ndbg.phtml'#
 URL_HALFYEAR_BASE ='http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/%s/page_type/zqbg.phtml'#
 URL_Q1_BASE ='http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/%s/page_type/yjdbg.phtml'
 URL_Q3_BASE ='http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/%s/page_type/sjdbg.phtml'
-f=open('stock_num.txt') 
-stock = [] 
-for line in f.readlines(): 
-    #print(line,end = '') 
-    line = line.replace('\n','') 
-    stock.append(line) 
-    #print(stock) 
-f.close() 
-print(stock) 
-for each in stock: 
-    url='http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/'+each+'/page_type/ndbg.phtml' 
-    req = urllib.request.Request(url) 
-    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0') 
-    page = urllib.request.urlopen(req) 
-    try: 
-        html = page.read().decode('gbk') 
-        target = r'&id=[_0-9_]{7}' 
-        target_list = re.findall(target,html) 
-        os.mkdir('./'+each) 
-        sid = each 
-        #print(target_list) 
-        for each in target_list: 
-            #print(a) #print(each) 
-            target_url='http://vip.stock.finance.sina.com.cn/corp/view/vCB_AllBulletinDetail.php?stockid='+sid+each 
-            #print(target_url) 
-            treq = urllib.request.Request(target_url) 
-            treq.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0') 
-            tpage = urllib.request.urlopen(treq) 
+TARGET_URL='http://vip.stock.finance.sina.com.cn/corp/view/vCB_AllBulletinDetail.php?stockid=%s%s'
+class ReportDownSina:
+    '''
+    this for report down load 
+    '''
+    def __init__(self,stockList):
+        self.stockList =stockList
+    def readStockListFromTxt(self,filename):
+        '''
+        add stock in the txt file like this
+        000651
+        000652
+        return list
+        '''
+        f=open(filename) #'stock_num.txt'
+        stock = [] 
+        for line in f.readlines(): 
+            line = line.replace('\n','') 
+            stock.append(line) 
+        f.close() 
+        print(stock) 
+        return stock
+    def readStockListFromXls(self,filename,colName='ts_code',suffix='1'):
+        '''
+        parameter is file name,stock column name ,suffix ='1' or '0',return suffix '1' or '0'
+
+        return list
+        '''
+        stockList =pd.read_excel(filename)
+        rlist =stockList[colName].tolist()
+        print (rlist)
+        rlist =self.listRemoveDup(rlist)
+        print (rlist)
+        returnList =[]
+        if(suffix):
+            for each in rlist:
+                returnList.append(each.split('.')[0])
+        print(returnList)
+    def listRemoveDup(self,inList):
+        newList =[]
+        for id in inList:
+            if id not in newList:
+                newList.append(id)
+        print (newList)
+        return newList
+    def reportDown(self,type='y'):
+        '''
+        parameter: 
+            'y'-year report;
+            'hf'- half year report
+            'q1' -quarter 1 report
+            'q3' -quarter 3 report
+
+        '''
+        if(type=='y'):
+            urlBase =URL_YEAR_BASE
+        if(type=='hy'):
+            urlBase =URL_HALFYEAR_BASE
+        if(type=='q1'):
+            urlBase =URL_Q1_BASE
+        if(type=='q3'):
+            urlBase =URL_Q3_BASE
+        for each in self.stockList: 
+            url=urlBase%each 
+            req = urllib.request.Request(url) 
+            req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0') 
+            page = urllib.request.urlopen(req) 
             try: 
-                thtml = tpage.read().decode('gbk') 
-                #print(thtml) 
-                file_url = re.search('http://file.finance.sina.com.cn/211.154.219.97:9494/.*?PDF',thtml) 
-                try: 
-                    #print(file_url.group(0)) 
-                    local = './'+sid+'/'+file_url.group(0).split("/")[-2]+'.pdf' 
-                    #调试用作文件占位 
-                    # #open(local, 'wb').write(b'success') 
-                    #print(local) 
-                    urllib.request.urlretrieve(file_url.group(0),local,None) 
-                except: 
-                    print('PDF失效;'+target_url) 
+                html = page.read().decode('gbk') 
+                target = r'&id=[_0-9_]{7}' 
+                target_list = re.findall(target,html) 
+                os.mkdir('./'+each) 
+                sid = each 
+                #print(target_list) 
+                for each in target_list: 
+                    #print(a) #print(each) 
+                    target_url=TARGET_URL%(sid,each) 
+                    #print(target_url) 
+                    treq = urllib.request.Request(target_url) 
+                    treq.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0') 
+                    tpage = urllib.request.urlopen(treq) 
+                    try: 
+                        thtml = tpage.read().decode('gbk') 
+                        #print(thtml) 
+                        file_url = re.search('http://file.finance.sina.com.cn/211.154.219.97:9494/.*?PDF',thtml) 
+                        try: 
+                            #print(file_url.group(0)) 
+                            local = './'+sid+'/'+file_url.group(0).split("/")[-2]+'.pdf' 
+                            #调试用作文件占位 
+                            # #open(local, 'wb').write(b'success') 
+                            #print(local) 
+                            urllib.request.urlretrieve(file_url.group(0),local,None) 
+                        except: 
+                            print('PDF失效;'+target_url) 
+                    except: 
+                        print('page of report  error:'+target_url) 
             except: 
-                print('年报下载页面编码错误;'+target_url) 
-    except: 
-        print('年报列表页面编码错误;'+url)
+                print('year report list error: '+url)
+
+if __name__ == '__main__':
+    rlist =[]
+    aa =ReportDownSina(rlist)
+    aa.readStockListFromXls('hs300.xls','code')
+
+
+# f=open('stock_num.txt') 
+# stock = [] 
+# for line in f.readlines(): 
+#     #print(line,end = '') 
+#     line = line.replace('\n','') 
+#     stock.append(line) 
+#     #print(stock) 
+# f.close() 
+# print(stock) 
+# for each in stock: 
+#     url='http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_Bulletin/stockid/'+each+'/page_type/ndbg.phtml' 
+#     req = urllib.request.Request(url) 
+#     req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0') 
+#     page = urllib.request.urlopen(req) 
+#     try: 
+#         html = page.read().decode('gbk') 
+#         target = r'&id=[_0-9_]{7}' 
+#         target_list = re.findall(target,html) 
+#         os.mkdir('./'+each) 
+#         sid = each 
+#         #print(target_list) 
+#         for each in target_list: 
+#             #print(a) #print(each) 
+#             target_url='http://vip.stock.finance.sina.com.cn/corp/view/vCB_AllBulletinDetail.php?stockid='+sid+each 
+#             #print(target_url) 
+#             treq = urllib.request.Request(target_url) 
+#             treq.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0') 
+#             tpage = urllib.request.urlopen(treq) 
+#             try: 
+#                 thtml = tpage.read().decode('gbk') 
+#                 #print(thtml) 
+#                 file_url = re.search('http://file.finance.sina.com.cn/211.154.219.97:9494/.*?PDF',thtml) 
+#                 try: 
+#                     #print(file_url.group(0)) 
+#                     local = './'+sid+'/'+file_url.group(0).split("/")[-2]+'.pdf' 
+#                     #调试用作文件占位 
+#                     # #open(local, 'wb').write(b'success') 
+#                     #print(local) 
+#                     urllib.request.urlretrieve(file_url.group(0),local,None) 
+#                 except: 
+#                     print('PDF失效;'+target_url) 
+#             except: 
+#                 print('年报下载页面编码错误;'+target_url) 
+#     except: 
+#         print('年报列表页面编码错误;'+url)
+
 
 # coding = UTF-8
 # 爬取自己编写的html链接中的PDF文档,网址：file:///E:/ZjuTH/Documents/pythonCode/pythontest.html

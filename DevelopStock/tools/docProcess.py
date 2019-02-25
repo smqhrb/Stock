5f59f7a39c70164211eec50e0075204a3e4551fb
smqhrb <smqhrb@163.com>
2019-02-26 06:58:03 +0800

    doc tool

--- DevelopStock/tools/docProcess.py
+++ DevelopStock/tools/docProcess.py
@@ -1,3 +1,6 @@
+'''
+pip install python_docx
+'''
 from docx import Document
 from docx.shared import Inches
 
@@ -20,19 +23,23 @@ document.add_paragraph(
     'first item in ordered list', style='ListNumber'
 )
 
-document.add_picture('monty-truth.png', width=Inches(1.25)) #插入图片
+document.add_picture('hello.png', width=Inches(1.25)) #插入图片
 
 table = document.add_table(rows=1, cols=3) #插入表格
 hdr_cells = table.rows[0].cells
 hdr_cells[0].text = 'Qty'
 hdr_cells[1].text = 'Id'
 hdr_cells[2].text = 'Desc'
-for item in recordset:
-    row_cells = table.add_row().cells
-    row_cells[0].text = str(item.qty)
-    row_cells[1].text = str(item.id)
-    row_cells[2].text = item.desc
-
+# recordset ={{'qty':1,'id':2,'desc':3}}
+# for item in recordset:
+#     row_cells = table.add_row().cells
+#     row_cells[0].text = str(item.qty)
+#     row_cells[1].text = str(item.id)
+#     row_cells[2].text = item.desc
+row_cells = table.add_row().cells
+row_cells[0].text = str(1)
+row_cells[1].text = str(2)
+row_cells[2].text = '3'
 document.add_page_break()
 
 document.save('demo.docx')  #保存文档

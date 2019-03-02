5696162e25b2bbbbf2da8b9fb0c6234663617037
smqhrb <smqhrb@163.com>
2019-03-03 01:06:55 +0800

    figure hide child axis lable

--- DevelopStock/testLearn/testLearn_matplot.py
+++ DevelopStock/testLearn/testLearn_matplot.py
@@ -17,121 +17,121 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import numpy as np
 
-# t = np.arange(0.01, 5.0, 0.01)
-# s1 = np.sin(2*np.pi*t)
-# s2 = np.exp(-t)
-# s3 = np.sin(4*np.pi*t)
+t = np.arange(0.01, 5.0, 0.01)
+s1 = np.sin(2*np.pi*t)
+s2 = np.exp(-t)
+s3 = np.sin(4*np.pi*t)
 
-# ax1 = plt.subplot(311)
-# plt.plot(t, s1)
-# plt.setp(ax1.get_xticklabels(), fontsize=6)
+ax1 = plt.subplot(311)
+plt.plot(t, s1)
+plt.setp(ax1.get_xticklabels(), fontsize=6)
 
-# # share x only
-# ax2 = plt.subplot(312, sharex=ax1)
-# plt.plot(t, s2)
-# # make these tick labels invisible
-# plt.setp(ax2.get_xticklabels(), visible=False)
+# share x only
+ax2 = plt.subplot(312, sharex=ax1)
+plt.plot(t, s2)
+# make these tick labels invisible
+plt.setp(ax2.get_xticklabels(), visible=False)
 
-# # share x and y
-# ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
-# plt.plot(t, s3)
-# plt.xlim(0.01, 5.0)
-# plt.show()
-import numpy as np
-import matplotlib.pyplot as plt
-
-# plt.axis([0, 100, 0, 1])
-# plt.ion()
-
-# xs = [0, 0]
-# ys = [1, 1]
-
-# for i in range(100):
-#     y = np.random.random()
-#     xs[0] = xs[1]
-#     ys[0] = ys[1]
-#     xs[1] = i
-#     ys[1] = y
-#     plt.plot(xs, ys)
-#     plt.pause(0.1)
-# plt.show()	
-
-fr = pd.Series(va.values.cumsum() / va.values.sum())
-va.plot(kind='bar')
-fr.plot(color='r',secondary_y=True, style='-o')
-plt.annotate(format(fr[7], '.2%'), xy=(7, fr[7]), xytext=(7*0.9, fr[7]*0.9),
-             arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=.2'))  # 用于注释图形指标
-# plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-# plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
-# plt1 = plt.pie(va.values,
-#                labels=va.index,
-#                autopct='%1.1f%%')
-plt.xticks(rotation=50)  # 设置字体大小和字体倾斜度
+# share x and y
+ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
+plt.plot(t, s3)
+plt.xlim(0.01, 5.0)
 plt.show()
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-y1 = []
-for i in range(50):
-    y1.append(i)  # 每迭代一次，将i放入y1中画出来
-    ax.cla()   # 清除键
-    ax.bar(y1, label='test', height=y1, width=0.3)
-    ax.legend()
-    plt.pause(0.1)
-plt.show()					
+# # plt.axis([0, 100, 0, 1])
+# # plt.ion()
 
+# # xs = [0, 0]
+# # ys = [1, 1]
 
-# plt.figure(figsize=(7,5))       # 确定图片大小
+# # for i in range(100):
+# #     y = np.random.random()
+# #     xs[0] = xs[1]
+# #     ys[0] = ys[1]
+# #     xs[1] = i
+# #     ys[1] = y
+# #     plt.plot(xs, ys)
+# #     plt.pause(0.1)
+# # plt.show()	
 
-# plt.subplot(211)                # 确定第一个图的位置
-# plt.plot(df.index,df['收盘价'].values,'b',label="1st")
-# plt.grid(True)
-# plt.axis('tight')
-# plt.ylabel('Values',size=20)
-# plt.title("000012")
-
-# plt.legend(loc=0)
-
-# plt.subplot(212)               # 确定第2个图的位置
-# plt.bar(df.index,df['成交量 （手）'].values,width = 0.2,color='g',label="2nd")  # 直方图的画法
-# plt.grid(True)
-# plt.xlabel("Time",size=20)
-# plt.ylabel("Volume",size=20)
-# plt.legend(loc=0)
-# # plt.gcf().autofmt_xdate()
+# fr = pd.Series(va.values.cumsum() / va.values.sum())
+# va.plot(kind='bar')
+# fr.plot(color='r',secondary_y=True, style='-o')
+# plt.annotate(format(fr[7], '.2%'), xy=(7, fr[7]), xytext=(7*0.9, fr[7]*0.9),
+#              arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=.2'))  # 用于注释图形指标
+# # plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+# # plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+# # plt1 = plt.pie(va.values,
+# #                labels=va.index,
+# #                autopct='%1.1f%%')
+# plt.xticks(rotation=50)  # 设置字体大小和字体倾斜度
 # plt.show()
 
-
-# 导入数据 - 注意：这里请填写数据文件在您电脑中的路径
-stock_data = pd.read_csv('stock data/sh600000.csv', parse_dates=[1])
-
-
- # 将数据按照交易日期从远到近排序
-stock_data.sort('date', inplace=True)
-
-
-
-
- # ========== 计算移动平均线
+# import matplotlib.pyplot as plt
+# fig, ax = plt.subplots()
+# y1 = []
+# for i in range(50):
+#     y1.append(i)  # 每迭代一次，将i放入y1中画出来
+#     ax.cla()   # 清除键
+#     ax.bar(y1, label='test', height=y1, width=0.3)
+#     ax.legend()
+#     plt.pause(0.1)
+# plt.show()					
 
 
-# 分别计算5日、20日、60日的移动平均线
-ma_list = [5, 20, 60]
+# # plt.figure(figsize=(7,5))       # 确定图片大小
+
+# # plt.subplot(211)                # 确定第一个图的位置
+# # plt.plot(df.index,df['收盘价'].values,'b',label="1st")
+# # plt.grid(True)
+# # plt.axis('tight')
+# # plt.ylabel('Values',size=20)
+# # plt.title("000012")
+
+# # plt.legend(loc=0)
+
+# # plt.subplot(212)               # 确定第2个图的位置
+# # plt.bar(df.index,df['成交量 （手）'].values,width = 0.2,color='g',label="2nd")  # 直方图的画法
+# # plt.grid(True)
+# # plt.xlabel("Time",size=20)
+# # plt.ylabel("Volume",size=20)
+# # plt.legend(loc=0)
+# # # plt.gcf().autofmt_xdate()
+# # plt.show()
 
 
- # 计算简单算术移动平均线MA - 注意：stock_data['close']为股票每天的收盘价
-for ma in ma_list:
-     stock_data['MA_' + str(ma)] = pd.rolling_mean(stock_data['close'], ma)
+# # 导入数据 - 注意：这里请填写数据文件在您电脑中的路径
+# stock_data = pd.read_csv('stock data/sh600000.csv', parse_dates=[1])
 
 
- # 计算指数平滑移动平均线EMA
- for ma in ma_list:
-     stock_data['EMA_' + str(ma)] = pd.ewma(stock_data['close'], span=ma)
+#  # 将数据按照交易日期从远到近排序
+# stock_data.sort('date', inplace=True)
 
 
- # 将数据按照交易日期从近到远排序
-stock_data.sort('date', ascending=False, inplace=True)
 
 
- # ========== 将算好的数据输出到csv文件 - 注意：这里请填写输出文件在您电脑中的路径
-stock_data.to_csv('sh600000_ma_ema.csv', index=False)
+#  # ========== 计算移动平均线
+
+
+# # 分别计算5日、20日、60日的移动平均线
+# ma_list = [5, 20, 60]
+
+
+#  # 计算简单算术移动平均线MA - 注意：stock_data['close']为股票每天的收盘价
+# for ma in ma_list:
+#      stock_data['MA_' + str(ma)] = pd.rolling_mean(stock_data['close'], ma)
+
+
+#  # 计算指数平滑移动平均线EMA
+#  for ma in ma_list:
+#      stock_data['EMA_' + str(ma)] = pd.ewma(stock_data['close'], span=ma)
+
+
+#  # 将数据按照交易日期从近到远排序
+# stock_data.sort('date', ascending=False, inplace=True)
+
+
+#  # ========== 将算好的数据输出到csv文件 - 注意：这里请填写输出文件在您电脑中的路径
+# stock_data.to_csv('sh600000_ma_ema.csv', index=False)
--- DevelopStock/tools/drawStockCurve.py
+++ DevelopStock/tools/drawStockCurve.py
@@ -1,90 +1,3 @@
-#####
-# '''
-
-
-#     12日EMA的计算：EMA12 = 前一日EMA12 X 11/13 + 今日收盘 X 2/13
-#     26日EMA的计算：EMA26 = 前一日EMA26 X 25/27 + 今日收盘 X 2/27
-#     差离值（DIF）的计算： DIF = EMA12 - EMA26，即为talib-MACD返回值macd
-#     根据差离值计算其9日的EMA，即离差平均值，是所求的DEA值。今日DEA = （前一日DEA X 8/10 + 今日DIF X 2/10），即为talib-MACD返回值signal
-#     DIF与它自己的移动平均之间差距的大小一般BAR=（DIF-DEA)x2，即为MACD柱状图。
-
-# '''
-# import pandas as pd
-# import numpy as np
-# import matplotlib as mpl 
-# from matplotlib.font_manager import FontProperties 
-# import matplotlib.pyplot as plt
-
-# def get_macd_data(df_raw,fast_period=12,slow_period=26,signal_period=9): 
-#     # df_raw['close'] -- 收盘价 
-#     # 收盘价按照日期升序( df_raw['date'] )排列 
-#     # 返回值都是 Series 
-#     fast_ewm=df_raw['收盘价'].ewm(span=fast_period).mean() 
-#     slow_ewm=df_raw['收盘价'].ewm(span=slow_period).mean() 
-#     dif=fast_ewm-slow_ewm 
-#     dea=dif.ewm(span=signal_period).mean() 
-#     # 一般概念里，macd柱是 (dif-dea)*2，实际上只是为了扩大显示效果 
-#     # # 实测后发现，也可以不乘以2，效果也足够清楚了 
-#     bar=(dif-dea)*2 
-#     # 将bar 分成红绿柱分别导出数据， 
-#     # #目的是后续用matplotlib绘图时，能够分色绘制 
-#     red_bar=bar[bar>=0] 
-#     green_bar=bar[bar<0] 
-#     return dif,dea,red_bar,green_bar
- 
-# title_font=FontProperties(family='YouYuan',size=18) 
-# mpl.rcParams['axes.unicode_minus']=False 
-# def draw_macd(df_raw, dif, dea, red_bar, green_bar, canvas_w=1000, canvas_h=480, xtick_period=20, title=u'MACD'): 
-#     '''
-#     •df_raw - 原始的从 csv 导出数据后生成的DataFrame，目的是用在生成major_xticks时定位使用
-#     •dif, dea, red_bar, green_bar 是使用 get_macd_data 方法计算出的MACD指标
-#     •canvas_w, canvas_h 为期望绘制出的图形宽度和高度，单位是像素
-#     •xtick_period 为生成x 方向刻度时，每间隔多少个数值，取一个值作为刻度显示出来——如果将全部日期显示出来，x方向的刻度将会是密密麻麻一片黑
-#     •title 是图形的标题
-#     '''
-#     dpi=72 
-#     figsize=canvas_w/72,canvas_h/72 
-#     plt.figure(figsize=figsize) 
-#     p_dif=plt.plot(dif.index,dif.values) 
-#     p_dea=plt.plot(dea.index,dea.values) 
-#     ####
-#     a =np.where(np.array(dif.values)>np.array(dea.values))
-#     print(a)
-#     print(dea.index[a[0][0]])
-#     print(dea.values[a[0][0]])
-#     # p_kdea =dea[np.array(dea.values)>np.array(dea.values)]
-#     # print(p_kdea)
-#     # for index in a:
- 
-
-#     plt.bar(red_bar.index, red_bar.values, color='#d62728') 
-#     plt.bar(green_bar.index, green_bar.values, color='#889966') 
-#     major_index=df_raw.index[df_raw.index%xtick_period==0] 
-#     major_xtics=df_raw['交易日期'][df_raw.index%xtick_period==0] 
-#     plt.xticks(major_index,major_xtics) 
-#     plt.legend((p_dif[0],p_dea[0]),[u'DIF',u'DEA']) 
-#     plt.title(title,fontproperties=title_font) 
-
-#     plt.annotate('local max', xy=(dea.index[a[0][0]], dea.values[a[0][0]]), xytext=(str(dea.index[a[0][0]]), str(dea.values[a[0][0]])),arrowprops=dict(facecolor='black', shrink=0.05))
-
-#     plt.show() 
-
-
-# zcfzb =pd.read_excel("000882.SZ.xls",'000882.SZ')
-# df_raw = zcfzb[['交易日期',  '收盘价','成交量 （手）']]
-# # df_raw=pd.read_csv(u'assets/招商银行.csv',usecols=['close','date','volume']) 
-# # 注意要排除无交易的单日数据 
-# df_raw[df_raw['成交量 （手）']==0]=np.nan 
-# df_raw.dropna() 
-# df_raw.sort_values(by='交易日期',ascending=True,inplace=True) 
-# # 注意，df_raw.index 必须是升序 
-# # 如果由于对 date 排序后，index变成了降序
-# # 要单独把index的顺序反过来 
-# df_raw.index=df_raw.index[::-1] 
-# dif,dea,red_bar,green_bar=get_macd_data(df_raw) 
-# draw_macd(df_raw=df_raw, dif=dif, dea=dea, 
-#             red_bar=red_bar, green_bar=green_bar, xtick_period=25, title=u'招商银行 MACD') 
-
 # 
 import numpy as np
 import pandas as pd
@@ -115,14 +28,22 @@ class stockCurve:
         self.data =data
         self.title_font=FontProperties(family='YouYuan',size=18) 
         mpl.rcParams['axes.unicode_minus']=False
+        self.xSplice =50
+        self.barWidth =0.4
 
     def format_date(self,x,pos=None): 
         if x<0 or x>len(self.date_tickers)-1: 
             return '' 
         tmp =self.date_tickers[int(x)]
-        strTmp =(tmp.astype('M8[D]').astype('O')).strftime("%Y-%m-%d")
+        strTmp =(tmp.astype('M8[D]').astype('O')).strftime("%Y%m%d")
 
         return strTmp
+
+    # def format_dateNull(self,x,pos=None): 
+    #     if x<0 or x>len(self.date_tickers)-1: 
+    #         return '' 
+    
+    #     return 'aaa'
 
     def drawKline(self,fig,ax):
         fig.subplots_adjust(bottom=0.1)
@@ -134,7 +55,7 @@ class stockCurve:
         # mpfData =tmpData.as_matrix()  
         mpfData=[tuple([i]+list(quote[1:])) for i,quote in enumerate(tmpData.values)] 
 
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(6)) 
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(self.xSplice)) 
         ax.xaxis.set_major_formatter(ticker.FuncFormatter(self.format_date)) 
         # ax.set_xticklabels(self.date_tickers)
         mpf.candlestick_ohlc(ax,mpfData,colordown='#53c156', colorup='#ff1717',width=0.2,alpha=1)
@@ -156,12 +77,16 @@ class stockCurve:
 
         p_dif=ax.plot(dif.index,dif.values) 
         p_dea=ax.plot(dea.index,dea.values) 
-        ax.bar(red_bar.index, red_bar.values, color='#d62728') 
-        ax.bar(green_bar.index, green_bar.values, color='#889966') 
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(6)) 
+        ax.bar(red_bar.index, red_bar.values,width = self.barWidth ,color='#d62728') 
+        ax.bar(green_bar.index, green_bar.values,width = self.barWidth ,color='#889966') 
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(self.xSplice)) 
         ax.xaxis.set_major_formatter(ticker.FuncFormatter(self.format_date)) 
+        for tick in ax.get_xticklabels():
+            tick.set_rotation(45)
 
         ax.legend((p_dif[0],p_dea[0]),[u'DIF',u'DEA']) 
+        # ax.annotate('test', xy=(p_dif[0],p_dea[0]), xytext=(p_dif[0],p_dea[0]),
+        #      arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=.2'))  # 用于注释图形指标
 
         plt.grid(True)
         
@@ -171,17 +96,15 @@ class stockCurve:
         # ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y%m%d'))
         colData =self.data
         self.date_tickers =colData.t.values
-        # colData['t'] = pd.to_datetime(colData['t'],format="%Y%m%d")
-        # colData['t']=colData['t'].apply(lambda x:dates.date2num(x)*1440)
-        # colData.set_index('t',inplace=True)
-        # plt.xticks(pd.date_range(colData.index[0],colData.index[-1],freq='M'),rotation=45)
-        # ax.set_xticklabels(self.date_tickers)
-        # ax1.plot(df.index,df['收盘价'],color='r')
-        ax.bar(colData.index,colData['volume'].values,width = 2,color='g',label="2nd")  # 直方图的画法
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(6)) 
-        ax.xaxis.set_major_formatter(ticker.FuncFormatter(self.format_date)) 
 
-        # plt.setp(ax.get_xticklabels())
+        red_bar=colData[colData['close']>=colData['open']] 
+        green_bar=colData[colData['close']<colData['open']] 
+
+        # ax.bar(colData.index,colData['volume'].values,width = self.barWidth,color='g',label="2nd")  # 直方图的画法
+        ax.bar(red_bar.index,red_bar['volume'].values,width = self.barWidth,color='r',label="2nd")
+        ax.bar(green_bar.index,green_bar['volume'].values,width = self.barWidth,color='g',label="2nd")
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(self.xSplice)) 
+        ax.xaxis.set_major_formatter(ticker.FuncFormatter(self.format_date)) 
         plt.grid(True)
        
     def drawAll(self):
@@ -189,13 +112,22 @@ class stockCurve:
         dif,dea,red_bar,green_bar=self.calcMacd()
         fig = plt.figure()
         ax0 = fig.add_subplot(311)
+
+
+        # ax0.axes.get_xaxis().set_visible(False)
         # fig,ax=plt.subplots(figsize=(1200/72,480/72))
         self.drawKline(fig,ax0)
+
         ax1 = fig.add_subplot(312,sharex=ax0)
+        # ax1.axes.get_xaxis().set_visible(False)
         self.drawColume(ax1)
+
         ax2 = fig.add_subplot(313,sharex=ax0)
         self.drawMacd(ax2,dif=dif, dea=dea, 
                     red_bar=red_bar, green_bar=green_bar, xtick_period=25, title=u'招商银行 MACD') 
+
+        plt.subplots_adjust(bottom=.12, top=.95, left=.10, right=.95,
+                        wspace=0.1, hspace=0.04)
         plt.show()
         pass
 

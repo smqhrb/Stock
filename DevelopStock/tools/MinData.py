2f2067d5cfad2830e9710300f33379262684f93d
smqhrb <smqhrb@163.com>
2019-03-16 05:45:25 +0800

    ui update

--- DevelopStock/tools/MinData.py
+++ DevelopStock/tools/MinData.py
@@ -17,7 +17,10 @@ from bs4 import BeautifulSoup
 import urllib.request as urllib2
 import getopt
 class StockMinuteData:
-    def __init__(self,destPath=".\\Minutes\\"):
+    def __init__(self,destPath=".\\Minutes\\",uiDlg =None):
+        self.uiDlg =uiDlg
+        # self.UiList =[]
+        # self.UiProcessBar =0
         self.destPath =destPath
         if os.path.exists(self.destPath):                     #检测路径是否存在,不存在则创建路径
             pass
@@ -38,7 +41,9 @@ class StockMinuteData:
                 time.sleep(random.randint(0,3))
                 break
             except:
-                print("超时重试") 
+                print("超时重试")
+                # self.UiList.append("超时重试") 
+                self.uiDlg.addListViewMessage("超时重试")
         return content
     def getStockLoop(self,code):
         '''
@@ -51,6 +56,7 @@ class StockMinuteData:
         startTime =startL.strftime("%Y-%m-%d %H:%M:%S")
         if(dateTimeNow<startTime):
             print('-----%s<%s,没有开始交易'%(dateTimeNow,startTime))
+            self.uiDlg.addListViewMessage('-----%s<%s,没有开始交易'%(dateTimeNow,startTime))
             return pd.DataFrame()
         sSpan =(now_time -startL)#计算距离 09:25:00 的时间
         idTSpan =math.ceil(sSpan.seconds/3.0)#每三秒一个界面 共同79个界面
@@ -61,6 +67,8 @@ class StockMinuteData:
         while(k<=idTSpan):
             data =self.getStockMiuteTrade(code,dateL,80-k,'1')
             percent = round(1.0 * k / idTSpan * 100,2)
+           
+            self.uiDlg.setProcessBarPos(percent)
             print('        当前进度 : %s [%d/%d]'%(str(percent)+'%',k,idTSpan),end='\r')
             dk =dk.append(data,ignore_index=True)
             k =k+1
@@ -68,8 +76,10 @@ class StockMinuteData:
         return df,dateL
     def getStockLoopToday(self,code):
         print("------开始读取股票(%s)今天的分时数据"%code)
+        self.uiDlg.addListViewMessage("------开始读取股票(%s)今天的分时数据"%code)
         dk,day =self.getStockLoop(code)
         print("------结束读取股票(%s)今天的分时数据"%code)
+        self.uiDlg.addListViewMessage("------结束读取股票(%s)今天的分时数据"%code)
         if(len(dk)<=0):
             return
         pathName ="%s%s(%s).xls"%(self.destPath,code,day)
@@ -77,9 +87,11 @@ class StockMinuteData:
         dk.to_excel(write,sheet_name=code,index=True)
         write.save()
         print("------股票(%s)今天的分时数据,写入%s"%(code,pathName))
+        self.uiDlg.addListViewMessage("------股票(%s)今天的分时数据,写入%s"%(code,pathName))
 
     def getStockLoopHistory(self,code,day):
         print("------开始读取股票(%s)%s的分时数据"%(code,day))
+        self.uiDlg.addListViewMessage("------开始读取股票(%s)%s的分时数据"%(code,day))
         now_time = datetime.datetime.now()
         destDay =datetime.datetime.strptime(day,"%Y-%m-%d")
         dayDelay =now_time - destDay
@@ -91,6 +103,7 @@ class StockMinuteData:
             while(k<=idTSpan):
                 data =self.getStockMiuteTrade(code,day,80 -k,'2')
                 percent = round(1.0 * k / idTSpan * 100,2)
+                self.uiDlg.setProcessBarPos(percent)
                 print('        当前进度 : %s [%d/%d]'%(str(percent)+'%',k,idTSpan),end='\r')
 
                 if(len(data)>0):
@@ -100,12 +113,14 @@ class StockMinuteData:
         else:
             dk,day =self.getStockLoop(code)
         print("------结束读取股票(%s)%s的分时数据"%(code,day))
+        self.uiDlg.addListViewMessage("------结束读取股票(%s)%s的分时数据"%(code,day))
         pathName ="%s%s(%s).xls"%(self.destPath,code,day)
         write = pd.ExcelWriter(pathName)
         # ========== 将算好的数据输出到xls文件 - 注意：这里请填写输出文件在您电脑中的路径
         dk.to_excel(write,sheet_name=code,index=True)
         write.save()  
         print("------股票(%s)%s的分时数据,写入%s"%(code,day,pathName))
+        self.uiDlg.addListViewMessage("------股票(%s)%s的分时数据,写入%s"%(code,day,pathName))
         return dk
       
    
@@ -234,7 +249,8 @@ class StockMinuteData:
             print(".................全部结束.................")
         else:
             print("...在文件中没有股票代码或者文件不存在 ,退出....")
-            exit()        
+            exit() 
+           
     def readStockList(self,fname=''):
         contents =[]
         f = open(fname)
--- DevelopStock/tools/MinDataUi.py
+++ DevelopStock/tools/MinDataUi.py
@@ -1,32 +1,80 @@
+# pyuic5 MinDataUi.ui -o MinDataUiBase.py
 import os
 from PyQt5 import QtCore, QtGui, QtWidgets
-from MinDataUiBase import Ui_Dialog
+from MinDataUiBase import Ui_dialog
 from PyQt5.QtCore import QStringListModel
-class MinDataUi_Dialog(Ui_Dialog):#QtWidgets.QWidget
+from PyQt5.QtCore import QDate
+import time
+from MinData import StockMinuteData
+from datetime import datetime, date, timedelta
+import threading
+class MinDataUi_Dialog(Ui_dialog):#QtWidgets.QWidget
     def setupUi(self,Dialog):
         Dialog.setObjectName("分时数据提取")
+        self.Main =StockMinuteData(uiDlg=self)
         super().setupUi(Dialog)
         self.dqljwj.clicked.connect(self.on_dqljwj_click)#self.myButton.clicked.connect(self.msg)  
         self.dqljwj_2.clicked.connect(self.on_dqljwj_2_click)
         self.listView.clicked.connect(self.on_listview_click)
         self.ljxz.clicked.connect(self.on_ljxz_click)
-        # self.dateEdit.setDate(QDate(2018, 12, 28))
+        #
+        now_time = datetime.now()#现在
+        end = now_time.strftime("%Y-%m-%d")
+        yesterday =now_time +timedelta(days = -1) 
+        start =yesterday.strftime('%Y-%m-%d')
+        self.dateEdit.setDate(QDate.fromString(start, 'yyyy-MM-dd'))#start time
+        self.dateEdit_2.setDate(QDate.fromString(end, 'yyyy-MM-dd'))#end time
+        # 
+        self.xzlsFs.clicked.connect(self.on_xzlsFs_click)
+        # 
+        # self.listView_2 = QtWidgets.QListView(self.groupBox)
+        #实例化列表模型，添加数据
+        self.qsL=QStringListModel()
+        self.Test=[]
+
+        #设置模型列表视图，加载数据列表
+        self.qsL.setStringList(self.Test)
+
+        #设置列表视图的模型
+        self.listView_2.setModel(self.qsL)
+        self.progressBar.setValue(0)
+
+    
+    def on_xzlsFs_click(self):
+        startDay =self.dateEdit.dateTime()
+        start =startDay.toString('yyyy-MM-dd')
+        endDay = self.dateEdit_2.dateTime()
+        end =endDay.toString('yyyy-MM-dd')
+        code =self.gpdmEdit.text()
+        if(len(code)==8):
+            t = threading.Thread(target=getDataWithDay, args=(code,start,end,self), name='funciton')
+            t.start()
+
+            # self.getDataWithDay(code,start,end)
+        
+
+
     def on_listview_click(self,qModelIndex):
         txtN =self.strlist[qModelIndex.row()]
         txtN =txtN[0:8]
         self.gpdmHb.setText(txtN)
+
     def on_ljxz_click(self):
         self.savePath = QtWidgets.QFileDialog.getExistingDirectory(self,  
                             "浏览",  
                             ".\\")
-        self.bclj_2.setText(self.savePath)
-       
+        self.Main.destPath =self.savePath+'/'
+        self.bclj_2.setText(self.Main.destPath)
+
+
 
     def on_dqljwj_click(self):
         download_path = QtWidgets.QFileDialog.getExistingDirectory(self,  
                             "浏览",  
                             ".\\")
         self.dqlj.setText(download_path)
+
+
     def on_dqljwj_2_click(self):
         path =self.dqlj.text()
         files = os.listdir(path)
@@ -41,6 +89,7 @@ class MinDataUi_Dialog(Ui_Dialog):#QtWidgets.QWidget
         #实例化列表模型，添加数据
         slm=QStringListModel()
         
+        
 
         #设置模型列表视图，加载数据列表
         slm.setStringList(self.strlist)
@@ -48,12 +97,70 @@ class MinDataUi_Dialog(Ui_Dialog):#QtWidgets.QWidget
         #设置列表视图的模型
         self.listView.setModel(slm)
 
-
-
+ 
+    def addListViewMessage(self,msg):
+        # self.item_1=QStandardItem(QIcon("./image/save.ico"), "普通员工A");
+        #  self.item_2 = QStandardItem(QIcon("./image/save.ico"), "普通员工B");
+ 
+        #  model=QStandardItemModel()
+        #  model.appendRow(self.item_1)
+        #  model.appendRow(self.item_2)
+        #  listView.setModel(model)
+        self.Test.append(msg)
+        #设置模型列表视图，加载数据列表
+        self.qsL.setStringList(self.Test)
         
+        pass
+    def setProcessBarPos(self,processBar):
+        self.progressBar.setValue(processBar)
+def getDataWithDay(code,start,end,uiL):
+    startDay =datetime.strptime(start,"%Y-%m-%d")
+    endDay =datetime.strptime(end,"%Y-%m-%d")
+    dayDelay = endDay -startDay
+    k=0
+    readDay =startDay
+    while(k<=dayDelay.days):
+        day =readDay.strftime('%Y-%m-%d')
+        readDay =readDay + timedelta(days=1)
+        uiL.Main.getStockLoopHistory(code,day)
+        k =k+1 
+    # def start_login(self):
+    #     # 创建线程
+    #     self.thread = Runthread()
+    #     # 连接信号
+    #     self.thread._signal.connect(self.callbacklog)
+    #     # 开始线程
+    #     self.thread.start()
+
+    # def callbacklog(self, msg):
+    #     # 奖回调数据输出到文本框
+    #     self.textEdit_log.setText(self.textEdit_log.toPlainText()+ "\n"+ msg+ "   "+
+    #                               time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()));
+
+# class Runthread(QtCore.QThread):
+#     # python3,pyqt5与之前的版本有些不一样
+#     #  通过类成员对象定义信号对象
+#     _signal = pyqtSignal(str)
+
+#     def __init__(self, parent=None):
+#         super(Runthread, self).__init__()
+
+#     def __del__(self):
+#         self.wait()
+
+#     def run(self):
+#         # 处理你要做的业务逻辑，这里是通过一个回调来处理数据，这里的逻辑处理写自己的方法
+#         # wechat.start_auto(self.callback)
+#         # self._signal.emit(msg);  可以在这里写信号焕发
+
+#     def callback(self, msg):
+#         # 信号焕发，我是通过我封装类的回调来发起的
+#         # self._signal.emit(msg);
+
         
 if __name__ == "__main__":
     import sys
+    
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
     ui = MinDataUi_Dialog()

9136a7941e7047dec63f37a72535872260cd2cd6
smqhrb <smqhrb@163.com>
2019-04-24 06:10:49 +0800

    getRSI

--- DevelopStock/tbUser/getRSI.py
+++ DevelopStock/tbUser/getRSI.py
@@ -26,22 +26,21 @@ def MainOpt():
     drawPic =""
     for o, a in opts:
         if o in ("-h", "--help"):
-            print("---------help content------------------------------------------------------")
-            print("|   -f fname          [get all stock data from config file]       ")
-            print("|   -a                [all stock data save to one file 'stock.xls']       ")
-            print("|   -s 20170111       [start time,format YYYYMMDD]                       ")
-            print("|   -e 20180111       [end   time,format YYYYMMDD]                 ")
+            print("---------帮助信息------------------------------------------------------")
+            print("|   -f fname    [从文件fname获取股票代码.当fname是all_stock时是获取市场上所有股票代码]")
+            print("|   -s 20170111 [起始日期,format YYYYMMDD]")
+            print("|   -e 20180111 [结束,format YYYYMMDD.如果是Now,就是到今天]")
                         
-            print("|   for example:")
-            print("|      example 1: all stock data write in one file 'stock.xls")
-            print("|           python main.py -a a -f stockList.txt -s 20170111 -e 20180111")
-            print("|      example 2: all stock data write in different files")
-            print("|           python main.py -f stockList.txt -s 20170111 -e 20180111")
-            print("|           python main.py -f stockList.txt -s 20170111 -e Now")
-            print("|      example 3: default time is now to now -365day")
-            print("|           python main.py -f stockList.txt")
+            print("|  例子:")
+            print("|      例1: 一个股票写入一个xls文件")
+            print("|           python getRSI.py -f stockList.txt -s 20170111 -e 20180111")
+            print("|           python getRSI.py -f all_stock -s 20170111 -e 20180111")
+            print("|           python getRSI.py -f stockList.txt -s 20150101 -e Now")
+            print("|           python getRSI.py -f all_stock -s 20150101 -e Now")
+            print("|      例2: 缺省时间是  一年前的今天 到 今天")
+            print("|           python getRSI.py -f stockList.txt")
             print("|                                           ")
-            print("|   stock code is 601800.SH or 300298.SZ or 000882.SZ")
+            print("|   股票代码格式 是 601800.SH 或者 300298.SZ 或者 000882.SZ")
             print("---------------------------------------------------------------------------------")
             sys.exit()
         elif o in ("-f", "--files"):
@@ -55,23 +54,17 @@ def MainOpt():
         else:
             sys.exit()
 
-    print("Please confirm xls file not to be covered in current folder")
-    print("Continue? Please input 'y' to continune,'n' to end this program.")
-    str = input("Enter your input: ")
+    print("在当前目录下请确认没有xls文件被覆盖.续输入y,结束输入n?")
+    str = input("请输入: ")
     if str=='n':
         exit()
     elif str=='y':
         pass
-    files="stockList.txt"
-    start ="20150101"
-    end ="Now"
+
     if len(files)>0:
         print("configure file name ="+files)
         if len(start)==0 and len(end)==0:
             now_time = datetime.datetime.now()
-            # end =now_time.strftime('%Y-%m-%d')
-            # lastyear_time =now_time -timedelta(days=365)
-            # start =lastyear_time.strftime('%Y-%m-%d')
             end =now_time.strftime('%Y%m%d')
             lastyear_time =now_time -timedelta(days=365)
             start =lastyear_time.strftime('%Y%m%d')
@@ -106,33 +99,38 @@ def getStockDataInDifferentFile(stockCodeList,start,end):
     if(stockCodeList ==""):
         data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
         stockCodeList =data.ts_code
+    inputConfirm =""
     for code in (stockCodeList):
-    # for code in stockCodeList:
         savefileName =path+'/'+code+'.xls'
         i =i+1
-        time.sleep(0.3)
+        
         if(os.path.exists(savefileName)):
             print("...[%d]%s exist"%(i,savefileName))
+            if(len(inputConfirm)<=0):
+                inputConfirm = input("更新最新的数据到已经存在的文件中吗？更新输入y,不更新输入n: ")
+            if inputConfirm=='n':
+                continue
+            elif inputConfirm=='y':
+                pass
             df0 =pd.read_excel(savefileName)
             df0 =df0.drop(['indexU','RSI6','RSI12','RSI24'],axis=1)
             start =str(df0.trade_date.max()+1)
             print("...reading Stock ="+code+" from "+ start +" to "+end+" ...")
+            time.sleep(0.3)
             df1 = pro.daily(ts_code=code, start_date=start, end_date=end)
             df0 =df0.append(df1)
-            df0.reset_index()
-            df=df0.sort_values(by=['trade_date'])
+            df0 =df0.set_index(['trade_date'])
+            df0.index =df0.index.astype(int)
+            df0=df0.sort_index()
+            df =df0.reset_index()
             
             df['RSI6'] =RSI(df.close,6)
             df['RSI12'] =RSI(df.close,12)
             df['RSI24'] =RSI(df.close,24)        
-            
-            
         else:    
-            
             print("...reading Stock ="+code+" from "+ start +" to "+end+" ...")
-            # df =ts.get_hist_data(code,start=start,end=end)
-            # df.rename(columns={'date':'日期', 'open':'开盘价','high':'最高价','close':'收盘价','low':'最低价','volume':'成交量','price_change':'价格变动','p_change':'涨跌幅','ma5':'5日均价','ma10':'10日均价','ma20':'20日均价','v_ma5':'5日均量','v_ma10':'10日均量','v_ma20':'20日均量','turnover':'换手率'},inplace = True)
-
+             # df.rename(columns={'date':'日期', 'open':'开盘价','high':'最高价','close':'收盘价','low':'最低价','volume':'成交量','price_change':'价格变动','p_change':'涨跌幅','ma5':'5日均价','ma10':'10日均价','ma20':'20日均价','v_ma5':'5日均量','v_ma10':'10日均量','v_ma20':'20日均量','turnover':'换手率'},inplace = True)
+            time.sleep(0.3)
             df1 = pro.daily(ts_code=code, start_date=start, end_date=end)
             df=df1.sort_values(by=['trade_date'])
             lenTh =len(df)
@@ -141,18 +139,11 @@ def getStockDataInDifferentFile(stockCodeList,start,end):
             df['RSI6'] =RSI(df.close,6)
             df['RSI12'] =RSI(df.close,12)
             df['RSI24'] =RSI(df.close,24)      
-
-    
-            # df.drop(['index'],axis=1,inplace=True)
-
             #df.rename(columns={'ts_code':'股票代码','trade_date':'交易日期','open':'开盘价','high':'最高价','low':'最低价','close':'收盘价','pre_close':'昨收价','change':'涨跌额','pct_chg':'涨跌幅(未复权)','vol':'成交量 （手）','amount':'成交额(千元)'}, inplace = True)
-
         writer = pd.ExcelWriter(savefileName)
         df.to_excel(writer,sheet_name=code)
-
         writer.save()
         print("...[%d]finish writing Stock =%s data to %s"%(i,code,savefileName))
-
 
 def getStockDataInOneFile(stockCodeList,fname,start,end):
     writer = pd.ExcelWriter(fname)
@@ -241,13 +232,3 @@ def calcRSI(path,fn):
 
 if __name__ == '__main__':
     MainOpt()
-    # getIndexDay('000001.SH','000001Index.xls','20180201','20180411')	
-	# dataProcess('000001.xls','000001')
-	# dataProcess('399001.xls','399001')
-    # walkDirData('./20170601To20190131')
-    #dataRead('.','002230_1.xls')
-    # dataRead('.','002460_1.xls')
-    # dataRead('.','603799_1.xls')
-    # calcRSI('.','002460_1.xls')
-    #dataProcess('002024SZ.xls','002024SZ')
-    #

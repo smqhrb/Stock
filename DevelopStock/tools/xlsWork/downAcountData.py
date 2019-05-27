5707bb5f3bd8d173e4117d1b86e61c7c9f0232bf
smqhrb <smqhrb@163.com>
2019-05-28 06:23:51 +0800

    update downAcountData

--- DevelopStock/tools/xlsWork/downAcountData.py
+++ DevelopStock/tools/xlsWork/downAcountData.py
@@ -25,47 +25,59 @@ class AccountPd:
         self.filename =""
         self.text =""
 
-        self.wb = xlwt.Workbook()                            #生成xls表
-        self.wsZcfzb = self.wb.add_sheet(u'资产负债表')       #填加 资产负债表 
-        self.wsLrb = self.wb.add_sheet(u'利润表')             #填加 利润表 
-        self.wsXjllb = self.wb.add_sheet(u'现金流量表')       #填加 现金流量表 
-        self.sheet =self.wsZcfzb
+        # self.wb = xlwt.Workbook()                            #生成xls表
+        # self.wsZcfzb = self.wb.add_sheet(u'资产负债表')       #填加 资产负债表 
+        # self.wsLrb = self.wb.add_sheet(u'利润表')             #填加 利润表 
+        # self.wsXjllb = self.wb.add_sheet(u'现金流量表')       #填加 现金流量表 
+        # self.sheet =self.wsZcfzb
         self.destPath =destPath
         if os.path.exists(self.destPath):                     #检测路径是否存在,不存在则创建路径
+            print("current path=%s"%(os.getcwd()))
+            print("%s exist"%(self.destPath))
             pass
         else:
             os.mkdir(destPath)
+            print("current path=%s"%(os.getcwd()))
+            print("create %s"%(self.destPath))
 
-    def GetFullAcount(self,Code,Name,type ='year'):
+    def GetFullAcount(self,Code,Name,typeQ ='year'):
         '''
         按年度或者报告季读取报表
         code：股票代码
         name: 股票名称
-        type: year -按年度读取 ,其他 按报告季节
+        typeQ: year -按年度读取 ,其他 按报告季节
         '''
-        self.sheet = self.wsZcfzb
+        # self.sheet = self.wsZcfzb
         #资产负债表
-        if type=='year':
+        if typeQ=='year':
             nType ='?type=year'
         else:
             nType=''
         
         Url1 = 'http://quotes.money.163.com/f10/zcfzb_'+Code+'.html%s'%(nType) #资产负债表
-        self.GetZcfzb(Url1,Code)
+        zcfzb =self.GetZcfzb(Url1,Code)
+       
 
-        self.sheet = self.wsLrb
+        # self.sheet = self.wsLrb
         Url1 = 'http://quotes.money.163.com/f10/lrb_'+Code+'.html%s'%(nType) #利润表
-        self.GetZcfzb(Url1,Code)
+        lrb =self.GetZcfzb(Url1,Code)
 
-        self.sheet = self.wsXjllb
+        # self.sheet = self.wsXjllb
         Url1 = 'http://quotes.money.163.com/f10/xjllb_'+Code+'.html%s'%(nType) #现金流量表
-        self.GetZcfzb(Url1,Code)
-        Name =Name.replace('*', '')
-        if len(self.filename)<=0:
-            self.wb.save(self.destPath+Name+'('+Code+').csv')
-        else:
-            self.wb.save(self.destPath+self.filename+'_'+Name+'('+Code+').csv')
-
+        llb =self.GetZcfzb(Url1,Code)
+        # Name =Name.replace('*', '')
+        # if len(self.filename)<=0:
+        #     self.wb.save(self.destPath+Name+'('+Code+').csv')
+        # else:
+        #     self.wb.save(self.destPath+self.filename+'_'+Name+'('+Code+').csv')
+        
+        # zcfzb.to_excel("%s\%s(%s_%s_zcfzb).xlsx"%(self.destPath,Code,Name,typeQ),index_label=u'报告日期')
+        # lrb.to_excel("%s\%s(%s_%s_lrb).xlsx"%(self.destPath,Code,Name,typeQ),index_label=u'报告日期')
+        # llb.to_excel("%s\%s(%s_%s_llb).xlsx"%(self.destPath,Code,Name,typeQ),index_label=u'报告日期')
+        zcfzb.to_csv("%s\%s(%s_%s_zcfzb).csv"%(self.destPath,Code,Name,typeQ),index_label=u'报告日期',encoding='utf_8_sig')
+        lrb.to_csv("%s\%s(%s_%s_lrb).csv"%(self.destPath,Code,Name,typeQ),index_label=u'报告日期',encoding='utf_8_sig')
+        llb.to_csv("%s\%s(%s_%s_llb).csv"%(self.destPath,Code,Name,typeQ),index_label=u'报告日期',encoding='utf_8_sig')
+ 
     def GetZcfzb(self,url,code):
         '''
         url - 读取链接
@@ -90,28 +102,28 @@ class AccountPd:
             if k<=0:
                 cells =row.findAll("th")
                 
-                self.sheet.write(j, 0, cells[0].text)
+                # self.sheet.write(j, 0, cells[0].text)
                 data0.append(cells[0].text)
                 continue
-            self.sheet.write(j, 0, cells[0].text)  
+            # self.sheet.write(j, 0, cells[0].text)  
             data0.append(cells[0].text)   
-        df =df.append(data0)   
-        print(df) 
+        # df =df.append(data0)   
+        # print(df) 
         #获取财务报表的数据
         table = soup.find("table",{"class":"table_bg001 border_box limit_sale scr_table"})
         df1 = pd.DataFrame()
         col_row=[]
-        data_row =[]
+        
         j=0
         for row in table.findAll("tr"):
             cells = row.findAll("td")
             j+=1
-            
+            data_row =[]
             if len(cells) > 0:#
                 i = 0
                 lencell = len(cells)#统计财务报表的年数            
                 while i < len(cells):
-                    self.sheet.write(j, i+1, cells[i].text)
+                    # self.sheet.write(j, i+1, cells[i].text)
                     data_row.append(cells[i].text)                                        
                     i=i+1
                 # df1 =df1.append(data1)
@@ -123,18 +135,43 @@ class AccountPd:
                 cells = row.findAll("th")
                 i=0
                 while i<len(cells):
-                    self.sheet.write(j,i+1,cells[i].text)
+                    # self.sheet.write(j,i+1,cells[i].text)
                     col_row.append(cells[i].text)  
-                   
                     i=i+1
 
-        print(df1)
-        return lencell
+        zcfzb = df1.T
+        # zcfzb.set_index('报告日期', inplace=True)
+        zcfzb=zcfzb.sort_index()
+        cols =zcfzb.columns.values.tolist()
+        for col in cols:
+            zcfzb[col] = zcfzb[col].str.replace(',','')
+            zcfzb[col] = zcfzb[col].str.replace('--','0')
+        zcfzb =zcfzb.apply(lambda col:pd.to_numeric(col, errors='coerce'))
+        zcfzb.fillna(0,inplace=True)
+        return zcfzb
 
+    def GetCodeList(self):
+        try:
+            print("------开始读取行业%s"%(classify))
+            ddf =ts.get_industry_classified()
+            ddf.to_csv('StockClass.csv')
+        except Exception as ex:
+            pass
 
-def test():
-    xlsTest =AccountPd()
-    xlsTest.GetFullAcount('000651','格力电器')
+def main():
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2]) 
+    print(sys.argv[3]) 
+    destPath ="%s\\Account"%(sys.argv[1])   
+    xlsTest =AccountPd(destPath=destPath)
+    code =sys.argv[2]
+    name =sys.argv[3]
+    if(code=='stock'):
+        xlsTest.GetCodeList()
+    else:
+        xlsTest.GetFullAcount(code,name,typeQ='year')
+        xlsTest.GetFullAcount(code,name,typeQ='quarter')
 
 if __name__ == '__main__':
-    test()
+    main()
